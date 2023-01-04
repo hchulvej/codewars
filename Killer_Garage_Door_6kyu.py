b5ce4d6dc16f44c86031e437da71c2dec3b1bd39
hchulvej <hchulvej@gmail.com>
2023-01-04 14:38:22 +0100

    CW

--- Killer_Garage_Door_6kyu.py
+++ Killer_Garage_Door_6kyu.py
@@ -36,6 +36,30 @@ def controller(events):
             self.stopped = True
             self.time = 0
         
+        def reverse_dir(self):
+            self.dir = (self.dir + 1) % 2
+        
+        def stop_start(self):
+            self.stopped = not self.stopped
+        
+        def move(self):
+            self.time = self.time + 1
+            if self.dir == 1:
+                self.position = min(self.position + 1, 5)
+                if self.position == 5:
+                    self.reverse_dir()
+            else:
+                self.position = max(self.position - 1, 0)
+                if self.position == 0:
+                    self.reverse_dir()
+        
+        def get_position(self):
+            return self.position
+        
+        
+            
+        
+        
     
     st = new State()
         
